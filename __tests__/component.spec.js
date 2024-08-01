97534182dc691a1c48355814ea6ad0c797cb988a
awim <taqwim@nalysa.com>
2024-08-01 09:16:04 +0700

    save new component spec and translate function

--- __tests__/component.spec.js
+++ __tests__/component.spec.js
@@ -1,14 +1,14 @@
-"use strict";
 const path = require("path");
 const assert = require("yeoman-assert");
 const helpers = require("yeoman-test");
 
+const componentName = "Awesome";
+
 describe("generator-wow-react:component", () => {
-  beforeEach(done => {
-    helpers
+  beforeEach(async () => {
+    await helpers
       .run(path.join(__dirname, "../generators/component"))
-      .withArguments(["Awesome"])
-      .on("end", done);
+      .withArguments([componentName]);
   });
 
   it("creates files", () => {
