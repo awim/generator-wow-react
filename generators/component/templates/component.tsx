7b65cc93d21ec70ede4dbce5c2e8605d13907167
awim <muhammadtaqwiem@gmail.com>
2024-09-21 20:30:19 +0700

    recomposing component to rendered smoothly by storybook

--- generators/component/templates/component.tsx
+++ generators/component/templates/component.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 import styles from './<%= name %>.module.scss';
 
 export type <%= componentName %>Props = {
+  /** replace this comment with prop's description for documentation */
   label?: string,
 }
 
-
-export const <%= componentName %> = ({label}: Readonly<<%= componentName %>Props>) => {
+/** 
+  * put description of <%= componentName %> component
+  * to allow react-docgen autogenerate the documentation
+*/
+export const <%= componentName %> = ({ label }: <%= componentName %>Props) => {
   return <div className={ styles._ }>{ label }</div>;
 }
 
 // https://reactjs.org/docs/typechecking-with-proptypes.html
 <%= componentName %>.defaultProps = {
   label: 'My awesome <%= componentName %> component',
-}
+} as <%= componentName %>Props;
